b503529473747fbb3862db77854a2fc78e97a0f6
Martin Bock <mail@mnbo.de>
2020-01-08 20:57:37 +0100

    Working on email notifier

---
+++ notification/__init__.py
@@ -0,0 +1,33 @@
+from jinja2 import FileSystemLoader, Environment
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from smtplib import SMTP
+
+
+class EmailNotifier:
+    BLOCKED_TEMPLATE = 'blocked_notification.html'
+
+    def __init__(self, config):
+        file_loader = FileSystemLoader('templates')
+        self.jinja_env = Environment(loader=file_loader)
+        self.config = config
+
+    def render_blocked_notification(self, recipient_name, blocked_result):
+        """Render the HTML template for the 'Blocked IP' notification."""
+        template = self.jinja_env.get_template(self.BLOCKED_TEMPLATE)
+        return template.render(
+            recipient_name=recipient_name,
+            table=blocked_result,
+            homepage_url=self.config['outlook']['links']['homepage'],
+            delisting_url=self.config['outlook']['links']['delisting'],
+        )
+
+    def build_email(self, recipient, sender, blocked_result):
+        message = MIMEMultipart()
+        message['Subject'] = ''
+        message['From'] = f"{recipient['name']} <{recipient['email']}>"
+        message['To'] = f"{sender['name']} <{sender['email']}>"
+        html = self.render_blocked_notification(recipient['name'], blocked_result)
+        message.attach(MIMEText(html, 'html'))
+        text_only = message.as_string()
+        return message, text_only
