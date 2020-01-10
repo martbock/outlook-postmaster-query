b4e68f791556b007bd03320f5be07a3e47c4ee0e
Martin Bock <mail@mnbo.de>
2020-01-10 14:55:15 +0100

    Implement correct MIMEMultipart alternative mode

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from smtplib import SMTPException
+
 import yaml
 from blessings import Terminal
 from config import Config, ValidationException
@@ -16,18 +18,25 @@ class App:
 
     def main(self):
         notifier = EmailNotifier(self.config)
+        recipient = self.config['email']['recipients'][0]
         email = notifier.build_email(
-            recipient=self.config['email']['recipients'][0],
-            sender=self.config['email']['sender'],
+            recipient=recipient,
             blocked_result=[{'first_ip': '1.1.1.1', 'last_ip': '9.9.9.9', 'blocked': True,
                              'details': 'Evidence of spamming'}]
         )
-        print(email)
+        try:
+            notifier.send(recipient_email=recipient['email'], message=email)
+        except SMTPException as e:
+            self.print_error('Sending email failed.', e, yaml_dump=False)
+            exit(1)
 
     def config_invalid(self, e: ValidationException):
-        print(f'{t.red + t.reverse} ERROR {t.normal + t.red} Your configuration is invalid.{t.normal}')
-        print(yaml.dump(e.errors))
+        self.print_error('Your configuration is invalid.', e.errors)
         exit(1)
+
+    def print_error(self, title, error, yaml_dump=True):
+        print(f'{t.red + t.reverse} ERROR {t.normal + t.red} {title + t.normal}')
+        print(yaml.dump(error) if yaml_dump else error)
 
 
 if __name__ == '__main__':
--- notification/__init__.py
+++ notification/__init__.py
@@ -12,7 +12,7 @@ class EmailNotifier:
         self.jinja_env = Environment(loader=file_loader, autoescape=select_autoescape())
         self.config = config
 
-    def render_blocked_notification(self, template_uri, recipient_name, blocked_result):
+    def _render_blocked_notification(self, template_uri, recipient_name, blocked_result):
         """Render the HTML template for the 'Blocked IP' notification."""
         template = self.jinja_env.get_template(template_uri)
         return template.render(
@@ -22,13 +22,29 @@ class EmailNotifier:
             delisting_url=self.config['outlook']['links']['delisting'],
         )
 
-    def build_email(self, recipient, sender, blocked_result):
-        message = MIMEMultipart()
-        message['Subject'] = ''
-        message['From'] = f"{recipient['name']} <{recipient['email']}>"
-        message['To'] = f"{sender['name']} <{sender['email']}>"
-        html = self.render_blocked_notification(self.BLOCKED_TEMPLATE['html'], recipient['name'], blocked_result)
-        txt = self.render_blocked_notification(self.BLOCKED_TEMPLATE['txt'], recipient['name'], blocked_result)
+    def build_email(self, recipient: dict, blocked_result: list):
+        """Build a MIME Multipart email that has both alternatives html and plain-text."""
+        sender = self.config['email']['sender']
+        message = MIMEMultipart(_subtype='alternative')
+        message['Subject'] = self.config['email']['subject']
+        message['From'] = f"{sender['name']} <{sender['email']}>"
+        message['To'] = f"{recipient['name']} <{recipient['email']}>"
+        html = self._render_blocked_notification(self.BLOCKED_TEMPLATE['html'], recipient['name'], blocked_result)
+        txt = self._render_blocked_notification(self.BLOCKED_TEMPLATE['txt'], recipient['name'], blocked_result)
         message.attach(MIMEText(html, 'html', _charset='utf-8' if not html.isascii() else None))
-        message.attach(MIMEText(txt, 'txt', _charset='utf-8' if not html.isascii() else None))
+        message.attach(MIMEText(txt, 'plain', _charset='utf-8' if not html.isascii() else None))
         return message.as_string()
+
+    def send(self, recipient_email: str, message: str):
+        """Send an email message to a recipient."""
+        connection = SMTP(
+            host=self.config['email']['smtp']['server'],
+            port=self.config['email']['smtp']['port']
+        )
+        connection.starttls()
+        connection.login(
+            user=self.config['email']['smtp']['user'],
+            password=self.config['email']['smtp']['password']
+        )
+        connection.sendmail(from_addr=self.config['email']['sender']['email'], to_addrs=recipient_email, msg=message)
+        connection.quit()
